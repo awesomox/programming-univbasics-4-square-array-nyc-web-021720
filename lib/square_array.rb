199a0b6087beecf93fefb3a91e75c281e9ce947b
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 20:36:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(array)
   results = []
   
   while counter < array.length()
-    results.unshift(array[counter] * array[counter])
+    results.push(array[counter] * array[counter])
     counter += 1 
   end
   p results
